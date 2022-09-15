a5ebc1fba05bef586e1107a9b84df59b1c0823c5
el bandingo <elbandingo@protonmail.com>
2022-09-15 21:13:17 -0230

    added styling to app.css. configured button on media at 960 px to show items at justify start which eliminated extra space. configured button to useEffect  on reload it wont show up again at smaller media screen size. Navbar complete for now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
-import React,  {useState} from 'react';
+import React,  {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from './Button';
+import { Button } from './Button.js';
 import './Navbar.css';
 
 
@@ -11,20 +11,27 @@ function Navbar() {
   const handleClick = ()=> setClick(!click);
   const closeMobileMenu = ()=> setClick(false);
 
-  const showButton = () => {
-    if(window.innerWidth <= 960) {
+  const showButton = ()=> {
+    if (window.innerWidth <= 960) {
       setButton(false);
     } else {
       setButton(true);
     }
+ 
+  };
 
-    window.addEventListener('resize' , showButton);
-  }
+  useEffect(()=> {
+    showButton()
+  }, [])
+
+  window.addEventListener('resize', showButton);
+
+
   return (
     <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to="/" className="navbar-logo">TRVL <i className='fab fa-typo3'/></Link>
+                <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>TRVL <i className='fab fa-typo3'/></Link>
                 <div className='menu-icon' onClick={handleClick}>
                   <i className={click ? 'fas fa-times' : 'fas fa-bars'} /> 
                 </div>
